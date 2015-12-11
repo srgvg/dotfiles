7936dc990a6693ce00601d8f92699dc67adb3905
Serge van Ginderachter <serge@vanginderachter.be>
2015-12-11 17:18:41 +0100

    modified:   bin/usbreset.sh

--- bin/usbreset.sh
+++ bin/usbreset.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 PWD=`dirname $0`
-USBRESET="usbreset"
+USBRESET="/home/serge/bin/usbreset"
 
 ID=$1
 MATCHES=$(lsusb | sed -n 's@Bus \([0-9]*\) Device \([0-9]*\): ID '$ID'.*@/dev/bus/usb/\1/\2@p')
 
-if [ -z ${MATCHES} ]; then
+if [ -z "${MATCHES}" ]; then
  sudo $USBRESET $ID
 else
  sudo $USBRESET $MATCHES
