5e6e1bfbe88b182fd010c6b5ab9a5d55ec0d1f56
Serge van Ginderachter <serge@vanginderachter.be>
2018-04-16 19:40:49 +0200

    switch i3bar to i3pystatus + various i3 config changes

---
+++ .i3/i3pystatusconf.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from i3pystatus import Status
+from i3pystatus.updates import aptget
+
+COLAU = "#66AAFF"
+COLAU2 = "#7799FF"
+COLHW = "#999999"
+COLIN = "#FFFFFF"
+
+status = Status()
+
+status.register("online",
+                format_online="",
+                color=COLAU,
+                format_offline="",
+                color_offline="#ff0000")
+
+status.register("shell",
+                ignore_empty_stdout=True,
+                color=COLIN,
+                command="i3pystatus-commands wifi",
+                interval=3)
+
+status.register("keyboard_locks",
+                color=COLIN,
+                format="{num}",
+                num_off="",
+                num_on="",)
+
+status.register("backlight",
+                color=COLIN,
+                transforms={'percentage': lambda cdict: round((cdict["brightness"] / cdict["max_brightness"]) * 10)},
+                format=" {percentage}",)
+
+status.register("clock",
+                color=COLIN,
+                format="%a %-d %B %Y %T",)
+
+status.register("updates",
+                format="APT: {count}",
+                color_working="#FF0000",
+                backends=[aptget.AptGet()])
+
+status.register("shell",
+                ignore_empty_stdout=True,
+                color=COLAU,
+                command="i3pystatus-commands audio_current_sink",
+                interval=3)
+
+status.register("pulseaudio",
+                sink=None,
+                color_unmuted=COLAU,
+                color_muted="",
+                format="🔊 {volume}%",
+                format_muted="🚫",)
+
+status.register("now_playing",
+                color=COLAU,
+                format="[🎶{status} {title} ({artist})]",
+                status={'pause': '', 'stop': '', 'play': ''},)
+
+status.register("shell",
+                ignore_empty_stdout=True,
+                color=COLAU,
+                command="i3pystatus-commands audio_sonos_volume",
+                interval=1)
+
+status.register("battery",
+                format=" {percentage:.0f}% {consumption:.1f}W {remaining:%E%hh:%Mm} {status}",
+                critical_level_percentage=10,
+                alert=True,
+                alert_percentage=5,
+                status={"DIS": "",
+                        "CHR": "",
+                        "FULL": "",},
+                full_color=COLHW,
+                charging_color="#ff7700",)
+
+status.register("temp",
+                color=COLHW,
+                format=" {temp}°C")
+
+status.register("load",
+                color=COLHW,
+                format=" {avg1} {avg5} {avg15} {tasks}")
+
+status.register("cpu_freq",
+                color=COLHW,
+                format=" {avgg}GHz")
+
+status.register("disk",
+                color=COLHW,
+                path="/",
+                format=" {percentage_avail}%",)
+
+status.register("mem",
+                color=COLHW,
+                format="🐏 {percent_used_mem}%",)
+
+status.register("network",
+                interface="enp0s31f6",
+                format_up="🖧  [\[ {v6cidr} \]] [{v4cidr}]",
+                format_down="",)
+
+status.register("network",
+                interface="wlp4s0",
+                format_up=" [{quality:3.0f}% '{essid}'] [\[ {v6cidr} \]] [{v4cidr}]",
+                format_down="",)
+
+
+status.run()
