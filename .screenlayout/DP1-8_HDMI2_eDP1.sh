b5275146cb6c48d5f80e433d2973598d54348565
Serge van Ginderachter <serge@vanginderachter.be>
2019-01-08 22:57:31 +0100

    switch to arandr-autoconfig

---
+++ .screenlayout/DP1-8_HDMI2_eDP1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 640x1440 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI1 --off --output HDMI2 --mode 2560x1440 --pos 2560x0 --rotate right --output DP1-1 --off --output DP1-8 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off
