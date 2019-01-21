75e07906b9da0a1f9c0f6b475a601ad6da82cde3
Serge van Ginderachter <serge@vanginderachter.be>
2019-01-21 17:03:52 +0100

    display stuff

--- .screenlayout/HDMI2_V813ZqnTN7_eDP1_4-OlRZBeYC.sh
+++ .screenlayout/HDMI2_V813ZqnTN7_eDP1_4-OlRZBeYC.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x1480 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI1 --off --output HDMI2 --mode 2560x1440 --pos 1920x0 --rotate right --output DP1-1 --off --output DP1-8 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DVI-I-1-1 --off --output HDMI2 --mode 2560x1440 --pos 1920x0 --rotate right --output HDMI1 --off --output DVI-I-2-2 --off --output DP2 --off
