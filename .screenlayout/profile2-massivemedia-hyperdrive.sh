b27fb5685a3a2b5f4bce091a137e37978526b20a
Serge van Ginderachter <serge@vanginderachter.be>
2019-10-13 17:26:33 +0200

    fregolo update

--- .screenlayout/profile2-massivemedia-hyperdrive.sh
+++ .screenlayout/profile2-massivemedia-hyperdrive.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 xrandr --output VIRTUAL1 --off
-xrandr --output eDP1 --mode 1920x1080 --pos 0x1080 --rotate normal
+xrandr --output eDP1 --mode 1920x1080 --pos 1920x0 --rotate normal --scale 0.7x0.7
 xrandr --output DP1 --off
-xrandr --output DP2-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-xrandr --output DP2-2 --off
+xrandr --output DP2-1 --off
+xrandr --output DP2-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 xrandr --output HDMI2 --off
 xrandr --output HDMI1 --off
 xrandr --output DP2 --off
