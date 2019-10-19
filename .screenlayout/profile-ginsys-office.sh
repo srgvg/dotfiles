2c9299750cbe76af87749cc252e43bfd79e29265
Serge van Ginderachter <serge@vanginderachter.be>
2019-10-19 14:25:53 +0200

    re-enable laptop screen at home office

--- .screenlayout/profile-ginsys-office.sh
+++ .screenlayout/profile-ginsys-office.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -x
 SCALE="0.7"
 xrandr --output VIRTUAL1 --off
-xrandr --output DP2-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --scale 1x1
 xrandr --output DP2-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --scale ${SCALE}x${SCALE}
 #xrandr --output eDP1 --mode 1920x1080 --pos 3840x1080 --rotate normal
-xrandr --output eDP1 --off
+#xrandr --output eDP1 --off
+xrandr --output eDP1 --mode 1920x1080 --pos 2688x432 --rotate normal  --scale ${SCALE}x${SCALE}
 xrandr --output DP1 --off
 xrandr --output DP2-1 --off
 xrandr --output DP2-3 --off
