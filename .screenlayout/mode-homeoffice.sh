fed46b431690ff796e51cd7cf9842a1f87f299ea
Serge van Ginderachter <serge@vanginderachter.be>
2020-08-22 14:16:16 +0200

    update

--- .screenlayout/mode-homeoffice.sh
+++ .screenlayout/mode-homeoffice.sh
@@ -4,7 +4,7 @@ xrandr --output VIRTUAL1 --off
 xrandr --output DP2-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --scale ${SCALE}x${SCALE}
 #xrandr --output eDP1 --mode 1920x1080 --pos 3840x1080 --rotate normal
 #xrandr --output eDP1 --off
-xrandr --output eDP1 --mode 1920x1080 --pos 2688x432 --rotate normal  --scale ${SCALE}x${SCALE}
+#xrandr --output eDP1 --mode 1920x1080 --pos 2688x432 --rotate normal  --scale ${SCALE}x${SCALE}
 xrandr --output eDP1 --off
 xrandr --output DP1 --off
 xrandr --output DP2-1 --off
