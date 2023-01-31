2c4e63d46bbc04ce6adede3a7c1de5cbfb00bfd1
Serge van Ginderachter <serge@vanginderachter.be>
2023-01-31 08:45:55 +0100

    minos at velleman update

--- bin/screenlayout/velleman_HDMI1_HBLLGqmQC8_HDMI2_llqcFkWEcc_eDP1_4-OlRZBeYC.sh
+++ bin/screenlayout/velleman_HDMI1_HBLLGqmQC8_HDMI2_llqcFkWEcc_eDP1_4-OlRZBeYC.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1920x1080 --pos 538x1080 --rotate normal 
-xrandr --output DP1 --off 
-xrandr --output DP2 --off 
-xrandr --output DP2-1 --off 
-xrandr --output DP2-2 --off 
-xrandr --output DP2-3 --off 
-xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal 
-xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal 
-xrandr --output VIRTUAL1 --off
+xrandr --output eDP1 --mode 1920x1080 --pos 913x1080 --rotate normal --output DP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
