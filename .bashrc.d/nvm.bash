00270730826d123a402ef4e0cdb93e047dd91898
Serge van Ginderachter <serge@vanginderachter.be>
2023-11-21 13:25:55 +0100

    update from fregolo

--- .bashrc.d/nvm.bash
+++ .bashrc.d/nvm.bash
@@ -1,5 +1,3 @@
-# this script takes more than 1.5 s to source
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
