d757fa7547b14bf4ea94b72b9d22bd747bdf8955
Serge van Ginderachter <serge@vanginderachter.be>
2023-12-23 16:55:10 +0100

    update from fregolo

--- .bashrc.d/broot.bash
+++ .bashrc.d/broot.bash
@@ -1,1 +1,2 @@
- hash broot >&/dev/null && eval "$(broot --print-shell-function bash)"
+source /home/serge/.config/broot/launcher/bash/br ||\
+hash broot >&/dev/null && eval "$(broot --print-shell-function bash)"
