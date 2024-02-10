c50cd754fef1555286d91e3a0cfbf36dbc8fc190
Serge van Ginderachter <serge@vanginderachter.be>
2024-02-10 20:53:22 +0100

    remove mateco

--- bin/screenlayout/MATECO_DP1_1920x1080.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --mode 1920x1080 --pos 0x1080 --rotate normal 
-xrandr --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal 
-xrandr --output DP2 --off 
-xrandr --output HDMI1 --off 
-xrandr --output HDMI2 --off 
-xrandr --output VIRTUAL1 --off
