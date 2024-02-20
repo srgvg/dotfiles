66236401024cf821b6f8772735699b66aeb4348f
Serge van Ginderachter <serge@vanginderachter.be>
2024-02-20 17:04:30 +0100

    update from goldorak

--- .bashrc.d/functions.bash
+++ .bashrc.d/functions.bash
@@ -87,6 +87,15 @@ function cohen() {
 	chmod -v +x "$(realpath $(git rev-parse --show-cdup))/.git/hooks/pre-commit"
 }
 
+function ct() {
+	if [ -n "$1" ] #&& [ $1 -gt 0 ]
+	then
+		column -t | sort --ignore-leading-blanks --key $1
+	else
+		column -t
+	fi
+}
+
 function hl() {
 	local pattern
 	local parameters
