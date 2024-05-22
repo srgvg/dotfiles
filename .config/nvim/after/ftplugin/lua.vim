792713477a06feaabc9c9542b2792cddb0a18bd1
Serge van Ginderachter <serge@vanginderachter.be>
2024-05-22 13:41:00 +0200

    update from goldorak

--- .config/nvim/after/ftplugin/lua.vim
+++
@@ -1,8 +0,0 @@
-" Disable inserting comment leader after hitting o or O or <Enter>
-set formatoptions-=o
-set formatoptions-=r
-
-nnoremap <buffer><silent> <F9> :luafile %<CR>
-
-" For delimitMate
-let b:delimitMate_matchpairs = "(:),[:],{:}"
--- .config/nvim/lua/config/treesitter.lua
+++
@@ -1,8 +0,0 @@
-require("nvim-treesitter.configs").setup {
-  ensure_installed = { "python", "cpp", "lua", "vim" },
-  ignore_install = {}, -- List of parsers to ignore installing
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { 'help' }, -- list of language that will be disabled
-  },
-}
