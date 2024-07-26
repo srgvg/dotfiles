fd3d56a8fd1a3a3cbd608a2d5d3f9c281f36f982
Serge van Ginderachter <serge@vanginderachter.be>
2024-07-26 17:12:49 +0200

    update from goldorak

---
+++ bin/goland.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# c-basic-offset: 4; tab-width: 4; indent-tabs-mode: t
+# vi: set shiftwidth=4 tabstop=4 noexpandtab:
+# :indentSize=4:tabSize=4:noTabs=false:
+
+source  ~/.bashrc.d/keychain.bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# shellcheck disable=SC1090
+source "$HOME/bin/common.bash"
+
+###############################################################################
+ASDF_PYTHONN_VERSION=3.11.9
+export ASDF_PYTHONN_VERSION
+~/Applications/GoLand/bin/goland.sh &
+
+# https://www.adangel.org/2024/06/21/nofile-limit-investigations/
+for pid in $( pgrep -f GoLand )
+do
+	prlimit --nofile=1024:1073741816 -p $pid
+done
+wait
