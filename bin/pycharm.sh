0debf558056814a0b6e47433f6f02d703330c112
Serge van Ginderachter <serge@ginsys.eu>
2025-04-07 21:08:23 +0200

    update from fregolo

--- bin/pycharm.sh
+++ bin/pycharm.sh
@@ -1,1 +1,24 @@
-../Documents/Applications/pycharm/bin/pycharm.sh
+#!/usr/bin/env bash
+
+# c-basic-offset: 4; tab-width: 4; indent-tabs-mode: t
+# vi: set shiftwidth=4 tabstop=4 noexpandtab:
+# :indentSize=4:tabSize=4:noTabs=false:
+
+source  ~/.bashrc.d/keychain.bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# shellcheck disable=SC1090
+source "$HOME/bin/common.bash"
+
+###############################################################################
+~/Applications/idea-IU/bin/idea.sh &
+
+# https://www.adangel.org/2024/06/21/nofile-limit-investigations/
+for pid in $( pgrep -f idea )
+do
+	prlimit --nofile=1024:1073741816 -p $pid
+done
+wait
