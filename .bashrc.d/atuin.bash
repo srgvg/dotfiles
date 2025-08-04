c18fe0b7d3178ee9d11faa8133146f4ca6967e89
Serge van Ginderachter <serge@ginsys.eu>
2025-08-04 22:40:09 +0200

    update from fregolo

--- .bashrc.d/atuin.bash
+++ .bashrc.d/atuin.bash
@@ -6,7 +6,10 @@ export ATUIN_NOBIND="true"
 eval "$(atuin init bash)"
 
 # bind to ctrl-r, add any other bindings you want here too
-bind -x '"\C-r": __atuin_history'
+# Only set up bindings in interactive shells
+if [[ $- == *i* ]]; then
+    bind -x '"\C-r": __atuin_history'
+fi
 
 # bind to the up key, which depends on terminal mode
 #bind -x '"\e[A": __atuin_history --shell-up-key-binding'
