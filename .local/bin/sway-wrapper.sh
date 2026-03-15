78a9adab74df96c62816508b219ee84c494ec522
Serge van Ginderachter <serge@ginsys.eu>
2026-03-15 01:31:08 +0100

    update from goldorak  step 2+3 before

--- .local/bin/sway-wrapper.sh
+++ .local/bin/sway-wrapper.sh
@@ -2,9 +2,9 @@
 # Source environment.d vars that GDM doesn't load
 eval "$(/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)"
 # Unset stale vars that were commented out
-unset WLR_DRM_NO_MODIFIERS
+unset WLR_DRM_NO_MODIFIERS WLR_DRM_NO_ATOMIC
 # Disable ibus to allow Sway XKB compose key to work
 # (environment.d can't set empty values, so we set them here)
 export GTK_IM_MODULE="" QT_IM_MODULE="" XMODIFIERS=""
-export WLR_RENDERER WLR_DRM_DEVICES VK_ICD_FILENAMES WLR_DRM_NO_ATOMIC
+export WLR_RENDERER WLR_DRM_DEVICES VK_ICD_FILENAMES WLR_SCENE_DISABLE_DIRECT_SCANOUT
 exec sway --unsupported-gpu "$@"
