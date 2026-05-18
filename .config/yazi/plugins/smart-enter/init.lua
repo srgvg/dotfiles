ab88b8d1f8eb3530c1179eac8382d84c4e36698e
Serge van Ginderachter <serge@ginsys.eu>
2026-05-18 16:55:54 +0200

    update from goldorak

--- .config/yazi/plugins/smart-enter/init.lua
+++
@@ -1,8 +0,0 @@
--- https://yazi-rs.github.io/docs/tips#smart-enter
---- @sync entry
-return {
-    entry = function()
-        local h = cx.active.current.hovered
-        ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
-    end,
-}
